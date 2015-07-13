d371e8bb40268ab775c00652a091ce77cf28f138
Elliott Seyler <emhs08@gmail.com>
2015-07-13 15:14:45 -0700

    Initial commit with lein output

---
+++ src/com/seriouslyseylerius/astronomy.clj
@@ -0,0 +1,6 @@
+(ns com.seriouslyseylerius.astronomy)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/com/seriouslyseylerius/astronomy_test.clj
@@ -0,0 +1,7 @@
+(ns com.seriouslyseylerius.astronomy-test
+  (:require [clojure.test :refer :all]
+            [com.seriouslyseylerius.astronomy :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
